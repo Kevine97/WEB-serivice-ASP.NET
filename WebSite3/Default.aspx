c7c95ac65b54ba28efb3e8f4add37fbc70f135ca
kevine97 <kevinesquivelrojas@hotmail.com>
2020-09-03 19:55:58 -0600

    Version final

---
+++ WebSite3/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <h1 class="text-center titulo">Servicio W E B</h1><hr />
+    <div class="row text_center">
+           <div class="col-lg-6">
+            <img src="../img/imagen23.png" class="img-fluid">
+            </div>
+
+    <div class="col-lg-4">
+    <div class="jumbotron ">
+        <h3 class="lead ke">Tipo de Cambio</h3><hr />
+        <p class="lead text-center">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript</p>
+        <p ><asp:TextBox ID="txtFecha" runat="server" Width="1229px" TextMode="Date" class="form-control"></asp:TextBox></p>
+        <p><asp:Button ID="Button1" runat="server"  Text="Consumir AHORA" class="btn btn-danger btn-lg" Width="1241px" OnClick="Button1_Click" /></p>
+        
+        <p class="text-center"> <asp:Label ID="Label1" class="label text-center" runat="server" Text="" ></asp:Label> 
+       <asp:Label ID="Label2" class="label text-center" runat="server" Text="" ></asp:Label></p>
+       
+    </div>
+        </div>
+    </div>
+
+    
+</asp:Content>
---
+++ WebSite3/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    ServiceReference1.Tipo_Cambio_BCNSoapClient WS = new ServiceReference1.Tipo_Cambio_BCNSoapClient();
+ 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //txtFecha.Text = DateTime.Now.ToString("yyyy-MM-dd");
+       
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (txtFecha.Text.Length > 0)
+       {
+            DateTime dia_Actual = Convert.ToDateTime(txtFecha.Text);
+            String ano = Convert.ToString(dia_Actual.Year);
+            String mes = Convert.ToString(dia_Actual.Month);
+            String dia = Convert.ToString(dia_Actual.Day);
+
+        Label1.Text = "Tipo de cambio en Córdobas por USD:" + "\t\t"+ WS.RecuperaTC_Dia(Convert.ToInt32(ano), Convert.ToInt32(mes), Convert.ToInt32(ano)).ToString();
+           switch (mes)
+           {
+               case "1":Label2.Text = "Fecha: "+dia+" de enero del "+ano; break;
+               case "2": Label2.Text = "Fecha: " + dia + " de febrero del " + ano; break;
+               case "3": Label2.Text = "Fecha: " + dia + " de marzo del " + ano; break;
+               case "4": Label2.Text = "Fecha: " + dia + " de abril del " + ano; break;
+               case "5": Label2.Text = "Fecha: " + dia + " de mayo del " + ano; break;
+               case "6": Label2.Text = "Fecha: " + dia + " de junio del " + ano; break;
+               case "7": Label2.Text = "Fecha: " + dia + " de julio del " + ano; break;
+               case "8": Label2.Text = "Fecha: " + dia + " de agosto del " + ano; break;
+               case "9": Label2.Text = "Fecha: " + dia + " de septiembre del " + ano; break;
+               case "10": Label2.Text = "Fecha: " + dia + " de ooctubre del " + ano; break;
+               case "11": Label2.Text = "Fecha: " + dia + " de noviembre del " + ano; break;
+               case "12": Label2.Text = "Fecha: " + dia + " de diciembre del " + ano; break;
+           }
+       }
+       else
+       {
+           Label1.Text = "Favor ingrese una fecha";
+           Label2.Text = "";
+       }
+
+
+
+
+    }
+}
